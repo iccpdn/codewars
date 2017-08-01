012157eaca3b309f818222448e3c29c6fdbab469
Ionut <iccpdn@gmail.com>
2017-08-01 19:21:17 +0300

    6th

---
+++ WhichAreIn.js
@@ -0,0 +1,26 @@
+//my version
+const inArray = (a1, a2) => {
+    const r = [];
+    a1.forEach((a1String) => {
+        a2.forEach((a2String) => {
+            if (a2String.includes(a1String) && !r.includes(a1String)) { 
+                r.push(a1String);
+            }
+        });
+    });
+
+    return r.sort();
+};
+
+//codewars best practice
+function inArray(arr1, arr2) {
+  return arr1.filter(function(needle) {
+    return arr2.some(function(haystack) {
+      return haystack.indexOf(needle) > -1;
+    });
+  }).sort();
+}
+
+const a2 = ['lively', 'alive', 'harp', 'sharp', 'armstrong'];
+const a1 = ['xyz', 'live', 'strong'];
+console.log(inArray(a1, a2));
