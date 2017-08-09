b40a8808eafcbacfa3720b8a209a7b99a5aa4675
Ionut <iccpdn@gmail.com>
2017-08-09 18:05:47 +0300

    8th

--- MergedStringChecker.js
+++ MergedStringChecker.js
@@ -1,26 +1,17 @@
-const isMerge = (s, part1, part2) => {
-    return !s ? !(part1 || part2) :
-        (s[0] === part1[0]/*?.*/ && isMerge(s.slice(1), part1.slice(1), part2)) ||
-        (s[0] === part2[0] && isMerge(s.slice(1), part1, part2.slice(1)));
+const isMerge = (string, part1, part2) => {
+    if (!string) return !(part1 || part2);
+    return (string[0] === part1[0] && isMerge(string.slice(1), part1.slice(1), part2)) ||
+           (string[0] === part2[0] && isMerge(string.slice(1), part1, part2.slice(1)));
 };
 
+// codewars best practice
 // function isMerge(s, part1, part2) {
-//   if (s === "" && part1 === "" && part2 === "") return true;
-//   if (s !== "") {
-//     if (s[0] === part1[0] && part1[0] === part2[0]) {
-//       // we make two branches of an recursion and get OR of the answers
-//       return (isMerge(s.slice(1), part1.slice(1), part2) || isMerge(s.slice(1), part1, part2.slice(1)));
-//     } else if (s[0] === part1[0]) {
-//       return isMerge(s.slice(1), part1.slice(1), part2);
-//     } else if (s[0] === part2[0]) {
-//       return isMerge(s.slice(1), part1, part2.slice(1));
-//     }
-//   }
-  
-//   return false;
+//   return !s ? !(part1 || part2) :
+//     s[0] == part1[0] && isMerge(s.slice(1), part1.slice(1), part2) ||
+//     s[0] == part2[0] && isMerge(s.slice(1), part1, part2.slice(1));
 // }
 
 isMerge('codewars', 'cdw', 'oears');
-// isMerge('codewars', 'code', 'wasr');
-// isMerge('codewars', 'cod', 'wars');
-// isMerge('Bahamas', 'Bam', 'haas');
+isMerge('codewars', 'code', 'wasr');
+isMerge('codewars', 'cod', 'wars');
+isMerge('Bahamas', 'Bam', 'haas');
